b02b8815bbb20bc13fb059cf3037c54e2cc5d0bd
Gionaldo <ghartono@sfu.ca>
2016-10-26 22:05:34 -0700

    approved dropdown

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
 	validates :tag_name, presence: true, uniqueness: true
+	validates :approved, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
