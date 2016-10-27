2b21df3733c424c26116e95db02c462f0ed55814
Louie Shen <louies@sfu.ca>
2016-10-26 20:51:07 -0700

    Add validation for user name, password, and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :user_name, presence: true
+	validates :password, presence: true
+	validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
