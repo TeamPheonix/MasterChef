3f4aa16c0af884ed07e6b4119a131deae62e3e76
rbaumert <rbaumert@sfu.ca>
2016-10-28 13:52:16 -0700

    switched to cmpt database (THIS PROBABLY NEEDS TO BE CHANGED DURING MERGE?)

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,3 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-match	':controller(/:action(/:id))',	:via	=>	:get
-match	':controller(/:action(/:id))',	:via	=>	:post
