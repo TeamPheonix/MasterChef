8f2d4cba864ddc746dec387444a6a040c2f4a1d1
rbaumert <rbaumert@sfu.ca>
2016-10-28 19:20:22 -0700

    added ingredients model,controller&migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028231346) do
+ActiveRecord::Schema.define(version: 20161029021424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.integer  "ingredient_id"
+    t.integer  "recipe_id"
+    t.string   "ingredient_name"
+    t.string   "web_link"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.integer  "recipe_id"
