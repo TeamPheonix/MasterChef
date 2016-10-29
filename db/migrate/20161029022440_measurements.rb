d5681266ad2dc2b11e6d0df95063856cb98ac8f3
rbaumert <rbaumert@sfu.ca>
2016-10-28 20:17:27 -0700

    messed around with measurements migration - still not working. leaving it for later

--- db/migrate/20161029022440_measurements.rb
+++ db/migrate/20161029022440_measurements.rb
@@ -1,8 +1,9 @@
 class Measurements < ActiveRecord::Migration[5.0]
-  def change
-    create_table :Measurements do |t|
-    t.string :measurement_type
+    def change
+      create_table :measurements do |t|
+      t.string :measurement_type
 
-    t.timestamps
+      t.timestamps
+    end
   end
 end
