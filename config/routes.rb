a94bb46f32bee2d3f3b83ee405c501437e64ee87
rbaumert <rbaumert@sfu.ca>
2016-11-02 18:11:47 -0700

    fixing syntax errors in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   get 'recipes/index'
 
-=======
   root 'welcome#index'
   get 'welcome/about'
   #login routes
@@ -22,6 +21,5 @@ Rails.application.routes.draw do
   post '/sessions/attempt_login'
 
   resources :users
->>>>>>> a553956bae4d498b63a2264657cc73046ed653b8
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
