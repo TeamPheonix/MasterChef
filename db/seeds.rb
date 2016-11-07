99cb534005d717305c9935886e4ecee5d308b7b8
ghartono <ghartono@sfu.ca>
2016-11-06 16:22:36 -0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(user_name: 'Extreme112', password: 'askdh',email: 'psanjuan06.sj@gmail.com', first_name: 'Patrick', last_name: 'SanJuan')
+User.create!(user_name: 'admin', password 'admin', email: 'admin@admin.com', first_name: 'admin', last_name:'admin', privileges:1)
