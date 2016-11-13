b3c50d0f6bac77a08e39a90ef447a693f1167837
Patrick San Juan <psanjuan06.sj@gmail.com>
2016-11-12 20:58:24 -0800

    Form, notice and redirect changes. - after signing up, user is redirecte to home with a notice popping up - password field uses 'password_field' when signinup up but uses 'text_field' if user is editing his profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,13 +41,14 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to root_path, notice: 'User was successfully created. Click Login on the top right' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # GET /users/1/edit
