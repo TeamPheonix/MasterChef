4679a1c9dc3818ff51a6502258c9dfccda2c4f0e
rbaumert <rbaumert@sfu.ca>
2016-11-21 20:51:11 -0800

    merging local develop with remote

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,19 +4,35 @@ class RecipesController < ApplicationController
   # GET /recipes
   # GET /recipes.json
   def index
-    @recipes = Recipe.all
+    #finding recipes by clicking tags
+    if params[:tag]
+      @tagged_id = TagsRecipe.where(tag_id: params[:tag])
+      i = 0
+      recipeid = []
+      until i == @tagged_id.size
+        recipeid[i] = @tagged_id[i].recipe_id
+        i = i+1
+      end
+      @recipes = Recipe.where(id: recipeid)
+    else
+      @recipes = Recipe.all
+    end
   end
 
   # GET /recipes/1
   # GET /recipes/1.json
   def show
-	  @tags_recipes = TagsRecipe.all
+    @tags_recipes = TagsRecipe.all
     @recipe_images = @recipe.recipe_images #returns an array of images for the current recipe
+    @tags_recipe = TagsRecipe.new
+    @tag = Tag.all
   end
 
   # GET /recipes/new
   def new
     @recipe = Recipe.new
+    @tags_recipe = TagsRecipe.new
+    @tag = Tag.all
     current_user
   end
 
