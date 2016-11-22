af8060d5e4a6c5eb18e9307910c237bb168c7899
Patrick San Juan <psanjuan06.sj@gmail.com>
2016-11-21 20:49:22 -0800

    Hardcoded gmail user and pass to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['masterchefgroup3'],
-   :password             => ENV['cmpt276phoenix'],
+   :user_name            => 'masterchefgroup3',
+   :password             => 'cmpt276phoenix',
    :openssl_verify_mode  => 'none',
    :authentication       => "plain",
    :enable_starttls_auto => true
