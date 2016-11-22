380f878d9793fab87d7c1ed5b204a083fc9cb96e
Patrick San Juan <psanjuan06.sj@gmail.com>
2016-11-21 20:43:37 -0800

    Uncommented openssl mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
    :port                 => 587,
    :user_name            => 'masterchefgroup3',
    :password             => 'cmpt276phoenix',
-   #:openssl_verify_mode  => 'none',
+   :openssl_verify_mode  => 'none',
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
