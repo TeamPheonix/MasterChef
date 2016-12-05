ebd36449605c56df41a02b967142a8ae10f2f32f
Patrick San Juan <psanjuan06.sj@gmail.com>
2016-12-04 19:21:44 -0800

    Set default value of impressions_count in recipes table to 0.

--- db/migrate/20161204193732_add_impressions_count_to_recipes.rb
+++ db/migrate/20161204193732_add_impressions_count_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddImpressionsCountToRecipes < ActiveRecord::Migration[5.0]
   def change
-    add_column :recipes, :impressions_count, :integer
+    add_column :recipes, :impressions_count, :integer, :default => 0
   end
 end
