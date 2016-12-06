5e1a7fb51f886457d751416b4e2f2d47807e4e54
ghartono <ghartono@sfu.ca>
2016-12-05 20:44:21 -0800

    One 's' to rule them all. Can delete now.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
 	validates :tag_name, presence: true, :uniqueness => {:case_sensitive => false}
 	validates :approved, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
-	has_many :tag_recipes, :dependent => :delete_all
+	has_many :tags_recipes, :dependent => :delete_all
 	has_many :recipes, -> {uniq}, :through => :tags_recipes
 end
